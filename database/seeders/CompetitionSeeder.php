38f2b09c1de4c76b57165bd8415a1c0f2ac9b16f
Joaquim Perez <jivkim@gmail.com>
2022-01-19 22:55:45 +0100

    php artisan make:model -a Competition

--- database/seeders/CompetitionSeeder.php
+++ database/seeders/CompetitionSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CompetitionSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\Competition::factory(1)->create();
     }
 }
