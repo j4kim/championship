0858054ca99ce2af2d6cff01492cc6716a32c24c
Joaquim Perez <jivkim@gmail.com>
2022-01-26 00:43:59 +0100

    list competitions in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <ul class="border-t">
+                        @foreach ($competitions as $competition)
+                        <a href="{{ url('/competition/' . $competition->id) }}">
+                            <li class="border-b py-2 hover:text-indigo-500">
+                                {{ $competition->id }}
+                                {{ $competition->name }}
+                            </li>
+                        </a>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
             <form class="py-10 text-center" action="{{ url('/competition') }}" method="post">
                 @csrf
-                <button class="py-2 px-6 font-semibold rounded-md bg-indigo-600  text-white">
+                <button class="py-2 px-6 font-semibold rounded-md bg-indigo-600 text-white">
                     <i class="fas fa-plus mr-2"></i> Nouvelle compétition
                 </button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', ['competitions' => Competition::all()]);
 })->middleware(['auth'])->name('dashboard');
 
 Route::post('/competition', function () {
