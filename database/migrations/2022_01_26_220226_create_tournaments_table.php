eb2e14f07ff12a86d7e2f4fea2ec078242d8fa65
Joaquim Perez <jivkim@gmail.com>
2022-01-26 23:18:41 +0100

    new match and tournament models and migrations

---
+++ database/migrations/2022_01_26_220226_create_tournaments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('competition_id')->constrained();
+            $table->date('start_date')->useCurrent();
+            $table->date('end_date')->nullable();
+            $table->foreignId('host_id')->constrained('users');
+            $table->string('place')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments');
+    }
+}
---
+++ database/migrations/2022_01_26_220629_create_matches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('player_1_id')->constrained('users');
+            $table->foreignId('player_2_id')->constrained('users');
+            $table->integer('player_1_score')->default(0);
+            $table->integer('player_2_score')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
