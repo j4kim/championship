fd41cff8c0a4ab5d087220bac46e91bf3e82d973
Joaquim Perez <jivkim@gmail.com>
2022-01-29 20:47:13 +0100

    mockup

--- resources/views/competition.blade.php
+++ resources/views/competition.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    Liste des rencontres
                     <ul class="border-t">
                         @foreach ($tournaments as $tournament)
                             <a href="{{ url("/tournaments/$tournament[id]") }}">
@@ -20,6 +21,7 @@
                     </ul>
                 </div>
             </div>
+            Classement ici
         </div>
     </div>
 
--- resources/views/tournament.blade.php
+++ resources/views/tournament.blade.php
@@ -11,9 +11,18 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="md:flex flex-row">
+                <div class="md:basis-1/3">Photos / conditions / menu ici</div>
+                <div class="md:basis-2/3">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 bg-white border-b border-gray-200">
+                            Classement ici
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div>
+                Liste des matches ici
             </div>
         </div>
     </div>
