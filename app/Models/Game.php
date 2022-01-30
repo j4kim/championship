b29fa5a3be0d8172676c9532c426ebc862075439
Joaquim Perez <jivkim@gmail.com>
2022-01-30 23:30:14 +0100

    mark the winner of a game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -9,11 +9,22 @@ class Game extends Model
 {
     use HasFactory;
 
+    protected $appends = ['player_1_wins', 'player_2_wins'];
+
+
     public function player1() {
         return $this->hasOne(User::class, 'id', 'player_1_id');
     }
 
     public function player2() {
         return $this->hasOne(User::class, 'id', 'player_2_id');
+    }
+
+    public function getPlayer1WinsAttribute() {
+        return $this->player_1_score > $this->player_2_score;
+    }
+
+    public function getPlayer2WinsAttribute() {
+        return $this->player_2_score > $this->player_1_score;
     }
 }
--- resources/views/components/game.blade.php
+++ resources/views/components/game.blade.php
@@ -1,21 +1,21 @@
 @props(['game'])
 
 <div class="flex mb-4 p-4 bg-white shadow-sm rounded-lg hover:shadow-md">
-    <div class="w-2/5">
+    <div class="w-2/5 {{ $game['player_1_wins'] ? 'font-bold text-indigo-600' : '' }}">
         {{ $game['player1']['name'] }}
     </div>
     <div class="w-1/5 flex">
-        <div class="w-1/3 text-right">
+        <div class="w-1/3 text-right {{ $game['player_1_wins'] ? 'font-bold text-indigo-600' : '' }}">
             {{ $game['player_1_score'] }}
         </div>
         <div class="w-1/3 text-center">
             -
         </div>
-        <div class="w-1/3">
+        <div class="w-1/3 {{ $game['player_2_wins'] ? 'font-bold text-indigo-600' : '' }}">
             {{ $game['player_2_score'] }}
         </div>
     </div>
-    <div class="w-2/5 text-right">
+    <div class="w-2/5 text-right {{ $game['player_2_wins'] ? 'font-bold text-indigo-600' : '' }}">
         {{ $game['player2']['name'] }}
     </div>
 </div>
