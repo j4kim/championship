faff56f9ac56163fd833351084a822e6d9b035d1
Joaquim Perez <jivkim@gmail.com>
2022-02-01 11:22:09 +0100

    [layout] responsive edit button

--- resources/views/tournament.blade.php
+++ resources/views/tournament.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex">
+        <div class="flex items-center">
             <h2 class="text-xl text-gray-800 leading-tight grow">
                 <a href="{{ url("/competitions/$competition[id]") }}" class="text-gray-500 hover:text-black">
                     {{ $competition["name"] }}
@@ -9,7 +9,8 @@
                 Tournoi #{{ $id }} du {{ $start_date }}
             </h2>
             <a href="{{ url("/tournaments/$id/edit") }}" class="text-sm py-2 px-4 -my-2 rounded-md text-indigo-600 border hover:border-indigo-400">
-                <i class="fas fa-edit mr-2 mt-1"></i> Modifier
+                <i class="fas fa-edit md:mr-2 mt-1"></i>
+                <span class="hidden md:inline">Modifier</span>
             </a>
         </div>
     </x-slot>
