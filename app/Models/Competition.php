670984b9a3e00940be17163651aec8a372123e33
Joaquim Perez <jivkim@gmail.com>
2022-02-02 12:34:30 +0100

    compute standings for a competition

--- app/Models/Competition.php
+++ app/Models/Competition.php
@@ -12,4 +12,29 @@ class Competition extends Model
     public function tournaments() {
         return $this->hasMany(Tournament::class);
     }
+
+    public function results() {
+        return $this->hasMany(Result::class);
+    }
+
+    public function getStandingsAttribute() {
+        $resultSumByUser = $this->results()
+            ->with('user')
+            ->selectRaw('sum(points) as points, user_id')
+            ->groupBy('user_id')
+            ->get()
+            ->sortByDesc('points')
+            ->values();
+
+        foreach ($resultSumByUser as $index => $current) {
+            $current->rank = $index + 1;
+            if ($index > 0) {
+                $previous = $resultSumByUser[$index - 1];
+                if ($previous->points == $current->points) {
+                    $current->rank = $previous->rank;
+                }
+            }
+        }
+        return $resultSumByUser;
+    }
 }
