8930e947333dc5814ef6c41b71a2bc6ea863c64b
Joaquim Perez <jivkim@gmail.com>
2022-02-02 11:48:32 +0100

    a new results table + model

---
+++ database/migrations/2022_02_02_104416_create_results_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('competition_id');
+            $table->foreignId('user_id');
+            $table->foreignId('tournament_id');
+            $table->unique(['competition_id', 'user_id', 'tournament_id']);
+            $table->integer('points');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
