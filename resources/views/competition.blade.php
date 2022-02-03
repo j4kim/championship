3218608739909636b34f520eea1e2e01b41cbe9f
Joaquim Perez <jivkim@gmail.com>
2022-02-03 13:54:55 +0100

    [layout]

--- resources/views/competition.blade.php
+++ resources/views/competition.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-5 gap-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-5 gap-12 items-start">
             <div class="bg-white shadow-sm sm:rounded-lg col-span-5 md:col-span-3">
                 <div class="p-4">
                     <div class="mb-4 text-xs text-gray-600">
