e65053e5de13bfec036c45f0fffc648cf71996aa
Joaquim Perez <jivkim@gmail.com>
2022-02-03 14:53:10 +0100

    no new lines in attribute component because it is rendered since the css whitespace rule

--- resources/views/components/attribute.blade.php
+++ resources/views/components/attribute.blade.php
@@ -1,8 +1,4 @@
 @props(['label', 'value'])
 
-<dt class="text-xs text-gray-600">
-    {{ $label }}
-</dt>
-<dd class="mb-2 whitespace-pre-line">
-    {{ $value }}
-</dd>
+<dt class="text-xs text-gray-600">{{ $label }}</dt>
+<dd class="mb-2 whitespace-pre-line">{{ $value }}</dd>
